16a72f7ab99038b40b5b591289125fdb2c61ed8f
Danielle Steinman <daniellesteinman@Danielles-MacBook-Air.local>
2020-11-01 08:38:06 -0500

    not working

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 const APIKey = "3590698d0ea6a356f08184a99ef7af1c";
 const searchBtn = $("#search-button");
 const searchValue = $("#search-value").val();
+const temperatureEL = $("#temp");
+const cityEl = $("#city");
 
 //Eventlistener for search button
 searchBtn.on("click", function (event) {
   //stops search button from refreshing page (?)
   event.preventDefault();
+  console.log("CLICKED");
 
-  // console.log("CLICKED");
-  const searchValue = $("#search-value").val();
-
-  const queryURL =
-    "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=" + APIKey;
-
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-    const results = response.data;
-    console.log(response);
-  });
 });
 
-// API and AJAX
-// const searchValue = $("#search-value").val();
+// pulls the weather from the API when user types in a city
+function todaysWeather() {
+    const queryURL =
+        "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=" + APIKey;
+    
+      $.ajax({
+        url: queryURL,
+        method: "GET",
+      }).then(function (response) {
+        const results = response.data;
+        console.log(response);
+    
+        // const city = response.name;
+        // const temp = response.main.temp;
+    
+        // temperatureEL.text(temp);
+        // cityEl.text(city);
+      });
+    };
 
-// const queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ searchValue + "&appid=" + APIKey;
-
-// $.ajax({
-//   url: queryURL,
-//   method: "GET",
-// }).then(function (response) {
-//   const results = response.data;
-//   console.log(results);
-// });
+// To convert to K to F
+// - 273.15) * 1.80 + 32).toFixed(2);
