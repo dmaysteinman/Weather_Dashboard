f129159636c811c3d713f850c5d84bb38f862b3e
Danielle Steinman <daniellesteinman@Danielles-MacBook-Air.local>
2020-11-01 10:32:45 -0500

    adding changes, same error message

--- script.js
+++ script.js
@@ -4,35 +4,35 @@ const searchValue = $("#search-value")
 const temperatureEL = $("#temp");
 const cityEl = $("#city");
 
+
 //Eventlistener for search button
 searchBtn.on("click", function (event) {
   //stops search button from refreshing page (?)
   let cityName = searchValue.val();
   event.preventDefault();
   console.log("CLICKED");
+  todaysWeather(cityName);
+
 });
 
 // pulls the weather from the API when user types in a city
 function todaysWeather(cityName) {
-    const queryURL =
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      searchValue +
-      "&appid=" +
-      APIKey;
-  
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-    }).then(function (response) {
-      console.log(response);
-  
-      const city = response.name;
-      const temp = response.main.temp;
-  
-      temperatureEL.text(temp);
-      cityEl.text(city);
-    });
-  }
+  const queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=" + APIKey;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+
+    const city = response.name;
+    const temp = response.main.temp;
+
+    temperatureEL.text(temp);
+    cityEl.text(city);
+  });
+}
 
 // To convert to K to F
 // - 273.15) * 1.80 + 32).toFixed(2);
