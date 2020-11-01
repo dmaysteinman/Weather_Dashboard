2f5519288a33a6a8d132d4c7495eae8545049a65
Danielle Steinman <daniellesteinman@Danielles-MacBook-Air.local>
2020-11-01 09:19:11 -0500

    setting parameters...still not working

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const APIKey = "3590698d0ea6a356f08184a99ef7af1c";
 const searchBtn = $("#search-button");
-const searchValue = $("#search-value").val();
+const searchValue = $("#search-value")
 const temperatureEL = $("#temp");
 const cityEl = $("#city");
 
 //Eventlistener for search button
 searchBtn.on("click", function (event) {
   //stops search button from refreshing page (?)
+  let cityName = searchValue.val();
   event.preventDefault();
   console.log("CLICKED");
 });
 
 // pulls the weather from the API when user types in a city
-function todaysWeather() {
+function todaysWeather(cityName) {
     const queryURL =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
       searchValue +
@@ -25,11 +26,11 @@ function todaysWeather() {
     }).then(function (response) {
       console.log(response);
   
-      // const city = response.name;
-      // const temp = response.main.temp;
+      const city = response.name;
+      const temp = response.main.temp;
   
-      // temperatureEL.text(temp);
-      // cityEl.text(city);
+      temperatureEL.text(temp);
+      cityEl.text(city);
     });
   }
 
