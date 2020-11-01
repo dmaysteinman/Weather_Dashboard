439bd8e36f823e80c6cf29b88dec473f7200cbb4
Danielle Steinman <daniellesteinman@Danielles-MacBook-Air.local>
2020-11-01 14:32:43 -0500

    added uvindex stuff and weather function

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Weather_Dashboard
+
+## Initial stages
+I have a lot of luck using resources as a template. Joel Dore and Shannon Quinn in our 5pm study group offered up their projects and I relied heavily on their hws to help guide me. I don't plan on making this a common practice but it's hard to deny the fact that seeing how the functions fit together in a working example helps me understand better than anything I learned in the lessons. That being said, I worked very hard to translate the codes into something that worked for me, in my own language. I hope that shows. 
+
+## Struggles
+I'm still confused about setting parameters and arguments in functions. I'd say it's probably my biggest source of frustration at this point. That being said, it's pretty heartening to see that I don't need to google simple things anymore and can now spontaneously create formulas that were nearly impossible a week ago. Another struggle is that I don't really know what a UV Index is but Austin has a really high one. 
--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 const APIKey = "3590698d0ea6a356f08184a99ef7af1c";
 const searchBtn = $("#search-button");
 const searchValue = $("#search");
-const temperatureEL = $("#temp");
-const cityEl = $("#city");
-
 
 //Eventlistener for search button
 searchBtn.on("click", function (event) {
-  //stops search button from refreshing page (?)
   let cityName = searchValue.val();
+  //stops search button from refreshing page (?)
   event.preventDefault();
   console.log("CLICKED");
   console.log(searchValue);
   console.log(cityName);
-  todaysWeather();
-
+  todaysWeather(cityName);
 });
 
-// pulls the weather from the API when user types in a city
+// pulls the weather info from the API when user types in a city
 function todaysWeather(cityName) {
   const queryURL =
     "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+APIKey;
@@ -26,14 +22,44 @@ function todaysWeather(cityName) {
     method: "GET",
   }).then(function (response) {
     console.log(response);
-
+    
     const city = response.name;
-    const temp = response.main.temp;
+    const tempF = (response.main.temp - 273.15) * 1.80 + 32; 
+    const humidity = response.main.humidity;
+    const lat = response.coord.lat;
+    const lon = response.coord.lon;
+    
+    
+    // Prints out weather info from response into card
+    $("#city").text("The weather in " + city + " today is:");
+    $("#temp").text("The temperature is " + tempF.toFixed(2) + " degrees Fahrenheit");
+    $("#humidity").text("The humidity is " + humidity);
+    
+    // gets UV Index
+    function getUVIndex(lat,lon) {
+      const queryURL = 
+        "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid="+ APIKey;
+        $.ajax({
+          url: queryURL,
+          method: "GET",
+        }).then(function (response) {
+          const uvIndex = response.value; 
+          console.log(uvIndex);
+          // prints out UVIndex info from response into card
+          $("#UVindex").text("The UV Index is " + uvIndex);
+          // colors the uvIndex
+          if (uvIndex <= 3) {
+            $("#UVindex").css("background-color", "green");
+          } else if (uvIndex >= 3 && uvIndex <= 5) {
+            $("#UVindex").css("background-color", "yellow");
+          } else if (uvIndex > 5 && uvIndex <= 7) {
+            $("#UVindex").css("background-color", "orange");
+          } else if (uvIndex > 7) {
+            $("#UVindex").css("background-color", "red");
+          }
+      });
+    };
+    getUVIndex(lat,lon)
+  });
+};
 
-    temperatureEL.text(temp);
-    cityEl.text(city);
-  });s
-}
-
-// To convert to K to F
-// - 273.15) * 1.80 + 32).toFixed(2);
